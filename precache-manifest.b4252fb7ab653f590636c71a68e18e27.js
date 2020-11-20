cbf7a9ab1deb5b6022626acf673789386a2d1620
Le Van Quoc Viet <61400407+lvqviet@users.noreply.github.com>
2020-11-20 23:05:15 +0700

    Updates

---
+++ precache-manifest.b4252fb7ab653f590636c71a68e18e27.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cfe52349c630056f7116edbf58f174e",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "86e06627ecbba37131e5",
+    "url": "/weather-app/static/css/main.919c036b.chunk.css"
+  },
+  {
+    "revision": "576735087da6f002f9e8",
+    "url": "/weather-app/static/js/2.51f5167e.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/weather-app/static/js/2.51f5167e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86e06627ecbba37131e5",
+    "url": "/weather-app/static/js/main.f03ab953.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "a8528a868d461f09ee29a47e4c5323c8",
+    "url": "/weather-app/static/media/Clear.a8528a86.png"
+  },
+  {
+    "revision": "11452cab98ccf2a171a36da25360ba1b",
+    "url": "/weather-app/static/media/Cloud-background.11452cab.png"
+  },
+  {
+    "revision": "c6d1cb3616b92442a99eb3acd4063a89",
+    "url": "/weather-app/static/media/LightCloud.c6d1cb36.png"
+  },
+  {
+    "revision": "b62e950be97488071f86695792d1a919",
+    "url": "/weather-app/static/media/Showers.b62e950b.png"
+  },
+  {
+    "revision": "e1202bb6505e7b4c7a9fac14eafe4162",
+    "url": "/weather-app/static/media/Sleet.e1202bb6.png"
+  }
+]);
